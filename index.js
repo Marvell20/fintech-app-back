8e64e1137ead99051505ee35d9ec2f91849d2887
Brian Coronel <bryancoronel200@gmail.com>
2022-03-12 19:40:59 -0500

    Some changes

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 const user = require('./routes/user.js');
 const express = require('express')
-// const oracledb = require('oracledb');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.use('/user', user);
 
-//   app.get('/usuario',function (req, res){
-//       let id = req.query.id;
-//       if (isNaN(id)){
-//           res.send('Query param id is not number')
-//           return
-//       }
-//       selectUsersById(req, res, id);
-//   })
 
 app.listen(port, () => console.log("listening on port %s!", port))
   
--- routes/user.js
+++ routes/user.js
@@ -32,4 +32,38 @@ route.get('/all', async (req, res) => {
     }
 })
 
+route.post('/create', async(req, res) => {
+  let connection;
+    try {
+      connection = await getdb();
+      const {nombres, apellidos, cedula, telefono, doc_id} = req.body;
+      
+      const sql = `INSERT INTO USUARIOS(nombres, apellidos, cedula, telefono, tipo_de_documentos_id) values(:nombres, :apellidos, :cedula, :telefono, :doc_id)`;
+      
+      await connection.execute(
+        sql,
+        {
+          nombres: nombres,
+          apellidos: apellidos,
+          cedula: cedula,
+          telefono: telefono,
+          doc_id: doc_id
+        },
+        { autoCommit: true}
+      );
+
+      res.send(req.body)
+
+    } catch (err) {
+      console.error(err);
+    } finally {
+      if (connection) {
+        try {
+          await connection.close();
+        } catch (err) {
+          console.error(err);
+        }
+      }
+    }
+})
 module.exports = route;
